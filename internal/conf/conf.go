46c43c38ed7d8ffc72a81d72e01cb097930aad47
Stanislav Bychkov <stanislav.bychkov67@gmail.com>
2021-04-23 21:58:30 +0300

    microservice was refactored

--- internal/conf/conf.go
+++ internal/conf/conf.go
@@ -3,9 +3,8 @@ package conf
 import "github.com/spf13/viper"
 
 type Config struct {
-	ServerLocalHost string `mapstructure:"SERVER_LOCALHOST"`
-	DB_URL string `mapstructure:"DB_URL"`
-
+	ServerLocalHost string `mapstructure:"ServerLocalhost"`
+	DBURL           string `mapstructure:"dbURL"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
--- postgres/postgres.go
+++ postgres/postgres.go
@@ -14,22 +14,13 @@ import (
 
 // TODO: context should be passed from request
 func SQLStatements(user models.User) {
-	config, err := conf.LoadConfig(".")
-	if err != nil {
-		log.Fatal("cannot load config:", err)
-	}
 
-	ctx := context.Background()
 	// TODO: pgxpool.Connect should be executed only once in main function
-	dbpool, err := pgxpool.Connect(ctx, config.DBSource)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer dbpool.Close()
+	dbpool := OpenConnection()
+	ctx := context.Background()
 
 	sqlAddUser := `INSERT INTO users (firstName, lastName, email) VALUES ($1, $2, $3)`
-	_, err = dbpool.Exec(ctx, sqlAddUser, user.FirstName, user.LastName, user.Email)
+	_, err := dbpool.Exec(ctx, sqlAddUser, user.FirstName, user.LastName, user.Email)
 	if err != nil {
 		// TODO: return fmt.Errorf( "Unable to insert data into database: %w", err)
 		fmt.Fprintf(os.Stderr, "Unable to insert data into database:: %v\n", err)
@@ -48,23 +39,15 @@ func SQLStatements(user models.User) {
 	}
 }
 
-/*func OpenConnection() *sql.DB {
-
+func OpenConnection() *pgxpool.Pool {
 	config, err := conf.LoadConfig(".")
 	if err != nil {
 		log.Fatal("cannot load config:", err)
 	}
-
-	db, err := sql.Open("postgres", config.DBSource)
+	pool, err := pgxpool.Connect(context.Background(), config.DBURL)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Unable to connection to database: %v\n", err)
 	}
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	return db
+	defer pool.Close()
+	return pool
 }
-*/
